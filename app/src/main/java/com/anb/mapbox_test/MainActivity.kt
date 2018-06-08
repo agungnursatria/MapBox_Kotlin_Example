2543dc3597295a34689a2a4cae04aafb8bba15c5
Agung Nursatria <agungnursatria@gmail.com>
2018-06-08 13:18:36 +0700

    adding navigation to maps

--- app/src/main/java/com/anb/mapbox_test/MainActivity.kt
+++ app/src/main/java/com/anb/mapbox_test/MainActivity.kt
@@ -12,7 +12,7 @@ import com.mapbox.android.core.location.LocationEngineProvider
 import com.mapbox.android.core.permissions.PermissionsListener
 import com.mapbox.android.core.permissions.PermissionsManager
 import com.mapbox.mapboxsdk.Mapbox
-import com.mapbox.mapboxsdk.annotations.IconFactory
+import com.mapbox.mapboxsdk.annotations.Marker
 import com.mapbox.mapboxsdk.annotations.MarkerOptions
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
 import com.mapbox.mapboxsdk.geometry.LatLng
@@ -21,18 +21,41 @@ import com.mapbox.mapboxsdk.maps.OnMapReadyCallback
 import com.mapbox.mapboxsdk.plugins.locationlayer.LocationLayerPlugin
 import com.mapbox.mapboxsdk.plugins.locationlayer.modes.CameraMode
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.*
-import com.mapbox.mapboxsdk.annotations.MarkerViewOptions
 
+// classes to calculate a route
+import com.mapbox.services.android.navigation.ui.v5.route.NavigationMapRoute;
+import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
+import com.mapbox.api.directions.v5.models.DirectionsResponse;
+import com.mapbox.api.directions.v5.models.DirectionsRoute;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
+import android.util.Log;
+import com.mapbox.geojson.Point
+import com.mapbox.mapboxsdk.plugins.locationlayer.modes.RenderMode
+
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncher
+import com.mapbox.services.android.navigation.ui.v5.NavigationLauncherOptions
 
 class MainActivity : AppCompatActivity(), LocationEngineListener, PermissionsListener {
 
-    lateinit var locationPlugin: LocationLayerPlugin
-    lateinit var locationEngine: LocationEngine
     lateinit var permissionsManager: PermissionsManager
     lateinit var mapboxMap: MapboxMap
+    var locationPlugin: LocationLayerPlugin? = null
+    var locationEngine: LocationEngine? = null
+    var originLocation: Location? = null
+    var destinationMarker: Marker? = null
+    var originCoord: LatLng? = null
+
+    // variables for calculating and drawing a route
+    var originPosition: Point? = null
+    var destinationPosition: Point? = null
+    var currentRoute: DirectionsRoute? = null
+    var navigationMapRoute: NavigationMapRoute? = null
+
+    val TAG = "DirectionsActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,42 +65,86 @@ class MainActivity : AppCompatActivity(), LocationEngineListener, PermissionsLis
             onCreate(savedInstanceState)
             getMapAsync(OnMapReadyCallback {
                 mapboxMap = it
+                enableLocationPlugin()
+
+                originCoord = LatLng(originLocation?.getLatitude()!!, originLocation?.getLongitude()!!)
                 mapboxMap.addOnMapClickListener {
                     onMapClick(it)
                 }
-//                initMarker()
-                enableLocationPlugin()
             })
+        }
+
+        btnStartNavigation.setOnClickListener {
+
+            var option = NavigationLauncherOptions.builder()
+                    .origin(originPosition)
+                    .destination(destinationPosition)
+                    .build()
+
+            NavigationLauncher.startNavigation(this@MainActivity, option)
         }
     }
 
-    fun onMapClick( latLng: LatLng ){
-        var str = String.format("User clicked at: %s", latLng.toString())
-        Toast.makeText(this@MainActivity, str, Toast.LENGTH_LONG).show();
+    private fun getRoute(origin: Point, destination: Point) {
+        NavigationRoute.builder()
+                .accessToken(Mapbox.getAccessToken())
+                .origin(origin)
+                .destination(destination)
+                .build()
+                .getRoute(object : Callback<DirectionsResponse> {
+                    override fun onResponse(call: Call<DirectionsResponse>, response: Response<DirectionsResponse>) {
+                        // You can get the generic HTTP info about the response
+                        if (response.body() == null) {
+                            Log.e(TAG, "No routes found, make sure you set the right user and access token.")
+                            return
+                        } else if (response.body()!!.routes().size < 1) {
+                            Log.e(TAG, "No routes found")
+                            return
+                        }
+
+                        // Mencari jarak terbaik dengan metode KNN, diambil [0] sebagai terbaik
+                        currentRoute = response.body()!!.routes()[0]
+
+                        // Draw the route on the map
+                        if (navigationMapRoute != null) {
+                            navigationMapRoute!!.removeRoute()
+                        } else {
+                            navigationMapRoute = NavigationMapRoute(null, mapView, mapboxMap, R.style.NavigationMapRoute)
+                        }
+                        navigationMapRoute!!.addRoute(currentRoute)
+                    }
+
+                    override fun onFailure(call: Call<DirectionsResponse>, throwable: Throwable) {
+                        Log.e(TAG, "Error: " + throwable.message)
+                    }
+                })
     }
 
-    fun initMarker() {
 
-        mapboxMap?.addMarker(MarkerOptions()
-                .position(LatLng(-6.8901086, 107.6172076))
-                .setTitle("Suitmedia Mobile")
-                .snippet("Jalan Sekeloa No. 2, Dipatiukur, Lebakgede, Coblong, Kota Bandung, Jawa Barat 40132"))
+    fun onMapClick(destinationCoord: LatLng) {
+        if (destinationMarker != null)
+            mapboxMap.removeMarker(destinationMarker!!)
 
-        mapboxMap?.addMarker(MarkerOptions()
-                .position(LatLng(-6.9740021, 107.6303814))
-                .setTitle("Universitas Telkom")
-                .snippet("Jl. Telekomunikasi No. 01, Terusan Buah Batu, Sukapura, Dayeuhkolot, Sukapura, Dayeuhkolot, Bandung, Jawa Barat 40257"))
+        destinationMarker = mapboxMap.addMarker(MarkerOptions()
+                .position(destinationCoord))
+
+        destinationPosition = Point.fromLngLat(destinationCoord?.getLongitude()!!, destinationCoord?.getLatitude()!!)
+        originPosition = Point.fromLngLat(originCoord!!.getLongitude(), originCoord!!.getLatitude())
+        getRoute(originPosition!!, destinationPosition!!)
+
+        if (!btnStartNavigation.isEnabled)
+            btnStartNavigation.isEnabled = true
     }
+
 
     fun enableLocationPlugin() {
         // Check if permissions are enabled and if not request
         if (PermissionsManager.areLocationPermissionsGranted(this)) {
             // Create a location engine instance
             initializeLocationEngine()
-            locationPlugin = LocationLayerPlugin(mapView, mapboxMap, locationEngine)
-            locationPlugin.setLocationLayerEnabled(true)
-            locationPlugin.setCameraMode(CameraMode.TRACKING)
-            lifecycle.addObserver(locationPlugin)
+
+            locationPlugin = LocationLayerPlugin(mapView, mapboxMap, locationEngine);
+            locationPlugin!!.setRenderMode(RenderMode.COMPASS);
         } else {
             permissionsManager = PermissionsManager(this)
             permissionsManager.requestLocationPermissions(this)
@@ -87,20 +154,21 @@ class MainActivity : AppCompatActivity(), LocationEngineListener, PermissionsLis
     @SuppressLint("MissingPermission")
     fun initializeLocationEngine() {
         locationEngine = LocationEngineProvider(this).obtainBestLocationEngineAvailable()
-        locationEngine.setPriority(LocationEnginePriority.HIGH_ACCURACY)
-        locationEngine.addLocationEngineListener(this)
-        locationEngine.activate()
-        val lastLocation = locationEngine.getLastLocation()
+        locationEngine!!.setPriority(LocationEnginePriority.HIGH_ACCURACY)
+        locationEngine!!.activate()
+
+        val lastLocation = locationEngine!!.getLastLocation()
         if (lastLocation != null) {
+            originLocation = lastLocation
             setCameraPosition(lastLocation!!)
         } else {
-            locationEngine.addLocationEngineListener(this)
+            locationEngine!!.addLocationEngineListener(this)
         }
     }
 
     private fun setCameraPosition(location: Location) {
         mapboxMap.animateCamera(CameraUpdateFactory.newLatLngZoom(
-                LatLng(location.getLatitude(), location.getLongitude()), 17.0))
+                LatLng(location.getLatitude(), location.getLongitude()), 16.0))
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
@@ -109,14 +177,15 @@ class MainActivity : AppCompatActivity(), LocationEngineListener, PermissionsLis
 
     override fun onLocationChanged(location: Location?) {
         if (location != null) {
+            originLocation = location;
             setCameraPosition(location);
-            locationEngine.removeLocationEngineListener(this);
+            locationEngine?.removeLocationEngineListener(this);
         }
     }
 
     @SuppressLint("MissingPermission")
     override fun onConnected() {
-        locationEngine.requestLocationUpdates();
+        locationEngine?.requestLocationUpdates();
     }
 
     override fun onExplanationNeeded(permissionsToExplain: MutableList<String>?) {
@@ -134,6 +203,8 @@ class MainActivity : AppCompatActivity(), LocationEngineListener, PermissionsLis
 
     override fun onStart() {
         super.onStart()
+        locationEngine?.requestLocationUpdates();
+        locationPlugin?.onStart();
         mapView.onStart()
     }
 
@@ -149,6 +220,8 @@ class MainActivity : AppCompatActivity(), LocationEngineListener, PermissionsLis
 
     override fun onStop() {
         super.onStop()
+        locationEngine?.removeLocationUpdates();
+        locationPlugin?.onStop();
         mapView.onStop()
     }
 
@@ -160,6 +233,7 @@ class MainActivity : AppCompatActivity(), LocationEngineListener, PermissionsLis
     override fun onDestroy() {
         super.onDestroy()
         mapView.onDestroy()
+        locationEngine?.deactivate();
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
